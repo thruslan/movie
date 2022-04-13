870db10e295ccbad2d5eeecae2196e815d194759
Ruslan <k4512084@gmail.com>
2022-04-13 16:09:47 +0600

    changed file

--- src/index.js
+++ src/index.js
@@ -2,22 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from "react-router-dom";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 ReactDOM.render(
   <BrowserRouter>
-    <LanguageContext.Provider value={option}>
         <App />
-    </LanguageContext.Provider>
-
   </BrowserRouter>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
