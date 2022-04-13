967fb21fd8b6bb39173b91032d332ac4b3f139f0
Ruslan <k4512084@gmail.com>
2022-04-13 15:50:46 +0600

    change

--- src/pages/NowPlaying.js
+++ src/pages/NowPlaying.js
@@ -3,7 +3,7 @@ import FilmCard from "../components/FilmCard";
 import axios from "axios";
 import {apiKey} from "../App";
 
-import {POPULAR_API} from "../Private";
+import {POPULAR_API} from "../App";
 
 const NowPlaying = () => {
     const [nowPlaying,setsNowPlaying] = useState([])
